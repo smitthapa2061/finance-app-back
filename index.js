d52f1f2bdac3869803d830efafc570ce7c188796
Smith Thapa <118682095+smitthapa2061@users.noreply.github.com>
2025-05-20 13:23:45 +0545

    u

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const bookingRoute = require("../back/route/bookingData.route.js");
+const bookingRoute = require("./route/bookingData.route.js");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
